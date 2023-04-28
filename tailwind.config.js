cfd5bf423ca1c29a8b84e4005bba4010fc62eb9a
Oliver Train <otrain@goodwood.com>
2023-04-28 15:15:29 +0100

    Edit to positioning of the 01,02 etc on cards and also footer integrated

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
     extend: {
       colors: {
         "grey-minor-numbers": "#13131366",
-        "grey-minor": "#131313cc"
+        "grey-minor": "#131313cc",
+        "grey-cta": "#ffffff99"
       },
       borderWidth: {
         "1": "1px"
@@ -14,11 +15,13 @@ module.exports = {
         "8": "30px"
       },
       padding: {
-        "13": "50px"
+        "13": "50px",
+        "15": "60px",
       },
       margin: {
         "15": "60px",
-        "8.5": "30px"
+        "8.5": "30px",
+        "5.5": "22px",
       },
     },
   },
