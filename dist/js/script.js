fbbdd3220fbf1a87d7618fbb2ef615e99c8f82e5
Oliver Train <olivertrain1996@gmail.com>
2023-05-18 13:07:28 +0100

    Edit to the sites pdf links
    corrected the use of src instead of href

--- dist/js/script.js
+++ dist/js/script.js
@@ -96,7 +96,7 @@ function displayedLogic(currentData) {
   let html = "";
   for (let i = 0; i < Object.keys(currentData).length; i++) {
     html += `
-      <a class="cursor-pointer" src="${currentData[i].pdfALink}" rel="nofollow">
+      <a class="cursor-pointer" target=”_blank” href="${currentData[i].pdfALink}"  rel="nofollow">
         <div class="guideline-cards">
           <div class="outerCardImageContainer">
             <img id="innerCardImage" class="w-48 p-4" src="${currentData[i].imageURL}">
