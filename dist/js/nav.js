91946de7837b7c73f62b351b5ffcb6e6952c7e82
Oliver Train <olivertrain1996@gmail.com>
2023-05-18 14:39:52 +0100

    Add load build date
    added build/last updated date

--- dist/js/nav.js
+++ dist/js/nav.js
@@ -5,3 +5,10 @@ button.addEventListener("click", mobileToggler);
 function mobileToggler() {
   mobileMenu.classList.toggle("menu-mb_class");
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  var buildDateElement = document.getElementById("buildDate");
+  var buildDate = new Date(document.lastModified);
+  var options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
+  buildDateElement.textContent = "Last Updated: " + buildDate.toLocaleString(undefined, options);
+});
