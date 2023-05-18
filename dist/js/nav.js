321488cf9724608a2d2ea520efa503ebb3301026
Oliver Train <olivertrain1996@gmail.com>
2023-05-18 14:51:19 +0100

    add changes

--- dist/js/nav.js
+++ dist/js/nav.js
@@ -5,10 +5,3 @@ button.addEventListener("click", mobileToggler);
 function mobileToggler() {
   mobileMenu.classList.toggle("menu-mb_class");
 }
-
-document.addEventListener("DOMContentLoaded", function() {
-  var buildDateElement = document.getElementById("buildDate");
-  var buildDate = new Date(document.lastModified);
-  var options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
-  buildDateElement.textContent = "Last Updated: " + buildDate.toLocaleString(undefined, options);
-});
